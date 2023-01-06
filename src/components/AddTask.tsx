788f383eef23dfa90fcd9dce8cb3de387a7d0bc0
MARCELO RODRIGUES <marcelorodriguesweb@gmail.com>
2023-01-06 18:41:02 -0300

    modification

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -4,11 +4,11 @@ import { Post } from './Post';
 import  styles  from './AddTask.module.css';
 import { ContentTask } from './ContentTask';
 
+
 export function AddTask() {
-
-    const [newTask, setNewTask] = useState([])
-    const [tasks, setTasks] = useState([
-
+    const [newTask, setNewTask] = useState<string>("")
+    const [tasks, setTasks] = useState<string[]>([
+      
     ])
 
     const [showElement, setShowElement] = useState(false)
@@ -21,9 +21,12 @@ export function AddTask() {
         setTasks([...tasks, newTask]);        
         setNewTask('');
     }
-    function handleNewTaskChange(event: ChangeEvent<HTMLLabelElement>){
+    function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>){
         event.target.setCustomValidity('')
         setNewTask(event?.target.value)        
+    }
+    function handleNewTaskInvalid(event: ChangeEvent<HTMLInputElement>){
+        event?.target.setCustomValidity('Esse campo é obrigatório!')
     }
     function deleteTask(taskToDelete: string){
         console.log(`Deletar task ${taskToDelete}`)
@@ -40,7 +43,10 @@ export function AddTask() {
                     className={styles.input} 
                     placeholder=" Adicione uma nova tarefa "
                     value={newTask}
-                    onChange={handleNewTaskChange}>
+                    onChange={handleNewTaskChange}
+                    onInvalid={handleNewTaskInvalid}
+                    required
+                >
                 </input>        
                 <button onClick={showOrHide} className={styles.button}>Criar <PlusCircle size='24' weight='bold'/></button>            
             </form>
